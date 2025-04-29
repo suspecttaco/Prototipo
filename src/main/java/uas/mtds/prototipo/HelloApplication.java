1d5f09f4f20149e958aa8ccc3bf32aadb9ea3f06
Suspect <50780739+suspecttaco@users.noreply.github.com>
2025-04-29 00:00:04 -0700

    Actualizado1 test.fxml

--- src/main/java/uas/mtds/prototipo/HelloApplication.java
+++ src/main/java/uas/mtds/prototipo/HelloApplication.java
@@ -11,9 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("1-POS.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(true);
         stage.show();
     }
 
