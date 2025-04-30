fc147f53c144c88fc1b9f2e9d69282f7b550f004
JCampos05 <juan.camposingsoft@gmail.com>
2025-04-29 18:58:15 -0700

    Actualizado1 test.fxml

--- src/main/java/uas/mtds/prototipo/HelloApplication.java
+++ src/main/java/uas/mtds/prototipo/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("5-CUPONES.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
