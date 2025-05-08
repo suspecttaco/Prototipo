232c9d50bdb29a0aa4b38994c2a16302e4c85477
suspecttaco <eladielgomez@gmail.com>
2025-05-08 00:20:15 -0700

    Update product examples in ProductService with detailed descriptions

--- src/main/java/uas/mtds/prototipo/ProductEngine/ProductService.java
+++ src/main/java/uas/mtds/prototipo/ProductEngine/ProductService.java
@@ -1,8 +1,9 @@
 package uas.mtds.prototipo.ProductEngine;
 
-import com.mysql.cj.log.NullLogger;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +12,17 @@ public class ProductService {
     // Ejemplo con datos estáticos
     public List<Product> obtenerProductosEjemplo() {
         List<Product> productos = new ArrayList<>();
-        productos.add(new Product("P001", "Café Americano",  30.00,"C","/uas/mtds/prototipo/products/americano.png"));
-        productos.add(new Product("P002", "Capuchino",  42.00, "C","/uas/mtds/prototipo/products/capuchino.png"));
-        /*
-        productos.add(new Product("P003", "Muffin",  20.00,"C","Chico","", "/uas/mtds/prototipo/images/muffin.png"));
-        productos.add(new Product("P004", "Sandwich",  45.00,"C","Chico","", "/uas/mtds/prototipo/images/sandwich.png"));
-        productos.add(new Product("P005", "Té Verde",  18.00,"C","Chico","","/uas/mtds/prototipo/images/te_verde.png"));
-        productos.add(new Product("P006", "Pastel",  30.00,"C","Chico","", "/uas/mtds/prototipo/images/pastel.png"));
-        productos.add(new Product("P007", "Galletas",  15.00,"C","Chico","", "/uas/mtds/prototipo/images/galletas.png"));
-        productos.add(new Product("P008", "Jugo",  22.00,"C","Chico","", "/uas/mtds/prototipo/images/jugo.png"));
-        productos.add(new Product("P009", "Refresco",  18.00,"C","Chico","", "/uas/mtds/prototipo/images/refresco.png"));
-        */
+
+        productos.add(new Product("P001", "Café Americano", 30.00, "/uas/mtds/prototipo/products/americano.png", "Café negro tradicional"));
+        productos.add(new Product("P002", "Capuchino", 42.00, "/uas/mtds/prototipo/products/capuchino.png", "Café con espuma de leche"));
+        productos.add(new Product("P003", "Muffin", 20.00, "/uas/mtds/prototipo/images/muffin.png", "Panecillo dulce horneado"));
+        productos.add(new Product("P004", "Sandwich", 45.00, "/uas/mtds/prototipo/images/sandwich.png", "Sándwich fresco"));
+        productos.add(new Product("P005", "Té Verde", 18.00, "/uas/mtds/prototipo/images/te_verde.png", "Té verde natural"));
+        productos.add(new Product("P006", "Pastel", 30.00, "/uas/mtds/prototipo/images/pastel.png", "Rebanada de pastel"));
+        productos.add(new Product("P007", "Galletas", 15.00, "/uas/mtds/prototipo/images/galletas.png", "Galletas horneadas"));
+        productos.add(new Product("P008", "Jugo", 22.00, "/uas/mtds/prototipo/images/jugo.png", "Jugo natural"));
+        productos.add(new Product("P009", "Refresco", 18.00, "/uas/mtds/prototipo/images/refresco.png", "Bebida carbonatada"));
+
         return productos;
     }
 
